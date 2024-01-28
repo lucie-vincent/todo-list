5cfd5a05e5d6adc95f11ef961480a578223b3e3f
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-28 20:00:24 +0100

    ajout des compteurs de tâche

--- js/main.js
+++ js/main.js
@@ -2,17 +2,23 @@
 // voir DOM - arbre du DOM
 // la méthode Document querySelector() renvoie le 1er Element qui correspond
 // au sélécteur spécifié (ici #btn)
-const addBtn = document.querySelector("#btn");
-const taskCard = document.querySelector(".todoCard");
-const taskContainer = document.querySelector("#todoCards");
-const delBtn = document.querySelector(".delBtn");
+const addBtn            = document.querySelector("#btn");
+const taskCard          = document.querySelector(".todoCard");
+const taskContainer     = document.querySelector("#todoCards");
+const delBtn            = document.querySelector(".delBtn");
+const compteurTaches    = document.querySelector("#compteurTaches");
+
+
 
 function addTask() {
+    // Màj du compteur de tâche pour le nom de chaque nouvelle tâche
+    countTasks++;
+
     // la méthode cloneNode() renvoie une copie du noeud sur lequel 
     // elle a été appelée 
     // true : les enfants du noeud doivent aussi être copié /
     // false : seul le noeud spécifié doit l'être
-    //clone la todoCard (taskCard)
+    // clone la todoCard (taskCard)
     const newTask = taskCard.cloneNode(true);    
     
     // on récupère le nouveau bouton de suppression
@@ -22,23 +28,26 @@ function addTask() {
         deleteTask(newTask);
     });
 
-
     // on récupère la nouvelle textarea 
     // puis on assigne le text de la nouvelle card en "New Task"
     const newTextArea = newTask.querySelector("textarea.task");
-    newTextArea.value = "New Task";
-
-    // on ajoute écouteur d'événement sur le nouveau bouton de la nouvelle
-    // tache
+    newTextArea.value = `Task ${countTasks}`;
 
     // on ajoute la nouvelle task créée en tant qu'enfant du taskContainer
     // !! on ajoute la nouvelle carte dans le DOM parce le taskContainer fait déjà partie du DOM
     taskContainer.appendChild(newTask);
-}
 
+    compterNbTaches();
+}
 
 function deleteTask(taskToDelete) {
     taskToDelete.remove();  // methode qui retire élément du DOM
+    compterNbTaches();
+}
+
+function compterNbTaches() {
+    let allTasks = document.querySelectorAll(".todoCard");
+    compteurTaches.value = allTasks.length;
 }
 
 // // EventTarget = interface DOM implémentée par des objets qui peuvent 
@@ -54,4 +63,9 @@ delBtn.addEventListener('click', function() {
     deleteTask(taskCard);
 });
 
+// Initialisation du compteur de tâches actives
+compterNbTaches();
+
+// Initialisation du compteur de tâche pour le nom de chaque nouvelle tâche
+let countTasks = parseInt(compteurTaches.value);
 
